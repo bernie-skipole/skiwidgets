b871220fc61afff91511c390ba3bf1fbf7f330da
Bernard Czenkusz <bernie@skipole.co.uk>
2022-09-26 10:18:17 +0100

    added info

---
+++ info/pagedescription.py
@@ -0,0 +1,27 @@
+import os
+
+from skipole import FailPage, GoTo, ValidateError, ServerError, ServeFile, PageData, SectionData
+
+
+def index(skicall):
+    "Sets up the page"
+
+    # the title and widget decription is in section 'header' which contains a
+    # HeadText widget with name 'title' and a TextBlockPara widget with name 'widgetdesc'
+    headersection = SectionData('header')
+    headersection['title', 'large_text'] = 'PageDescription'
+    # A textblock contains the widget description
+    ref = "widgets.info.PageDescription"
+    headersection['widgetdesc','textblock_ref'] = ref
+    headersection['widgetdesc','text_refnotfound'] = f'Textblock reference {ref} not found'
+    skicall.update(headersection)
+
+    # this code file contents is placed in section 'codefile' which contains a
+    # PreText widget with name 'pretext'
+    codesection = SectionData('codefile')
+    code = os.path.realpath(__file__)
+    with open(code) as f:
+        codesection['pretext', 'pre_text'] = f.read()
+    skicall.update(codesection)
+
+    
