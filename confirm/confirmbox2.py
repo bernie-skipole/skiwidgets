027b0aa7eb3860ef48088cdb00d077382389f8fb
Bernard Czenkusz <bernie@skipole.co.uk>
2022-11-06 19:07:21 +0000

    added ConfirmBox2 widget

---
+++ confirm/confirmbox2.py
@@ -0,0 +1,54 @@
+import os
+
+from skipole import FailPage, GoTo, ValidateError, ServerError, ServeFile, PageData, SectionData
+
+
+def index(skicall):
+    "Called by a SubmitData responder, and sets up the page"
+
+    # the title and widget decription is in section 'header' which contains a
+    # HeadText widget with name 'title' and a TextBlockPara widget with name 'widgetdesc'
+    # It also has a ButtonLink2 widget with name 'tomodule'
+    headersection = SectionData('header')
+    headersection['title', 'large_text'] = 'ConfirmBox2'
+    # A textblock contains the widget description
+    ref = "widgets.confirm.ConfirmBox2"
+    headersection['widgetdesc','textblock_ref'] = ref
+    headersection['widgetdesc','text_refnotfound'] = f'Textblock reference {ref} not found'
+    # link to this widgets module page
+    headersection['tomodule','button_text'] = "Module: confirm"
+    headersection['tomodule','link_ident'] = skicall.makepath('confirm')
+    skicall.update(headersection)
+
+    # this code file contents is placed in section 'codefile' which contains a
+    # PreText widget with name 'pretext'
+    codesection = SectionData('codefile')
+    code = os.path.realpath(__file__)
+    with open(code) as f:
+        codesection['pretext', 'pre_text'] = f.read()
+    skicall.update(codesection)
+
+
+
+def show_widget(skicall):
+    "Called by a button to show the confirmbox"
+    if ('buttonlink2', 'get_field1') not in skicall.call_data:
+       raise FailPage(message="Invalid call")
+
+    if skicall.call_data['buttonlink2', 'get_field1'] != 'show':
+      raise FailPage(message="Invalid call")
+
+    pd = PageData()
+    pd['confirmbox2', 'hide'] = False
+    pd['result', 'para_text'] = "Awaiting confirm action"
+    skicall.update(pd)
+
+
+def confirm(skicall):
+    "Responds to confirm submission from confirmbox2"
+
+    pd = PageData()
+    pd['confirmbox2', 'hide'] = True
+    pd['result', 'para_text'] = "The operation has been confirmed"
+    skicall.update(pd)
+
