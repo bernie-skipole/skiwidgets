bdae4ae070359cc7ccfcbf6e35a40eea3eb4a3c8
Bernard Czenkusz <bernie@skipole.co.uk>
2023-01-18 18:11:48 +0000

    Working on TableList widget

---
+++ lists/tablelist.py
@@ -0,0 +1,39 @@
+import os
+
+from skipole import FailPage, GoTo, ValidateError, ServerError, ServeFile, PageData, SectionData
+
+
+def index(skicall):
+    "Called by a SubmitData responder, and sets up the page"
+
+    # the title and widget decription is in section 'header' which contains a
+    # HeadText widget with name 'title' and a TextBlockPara widget with name 'widgetdesc'
+    # It also has a ButtonLink2 widget with name 'tomodule'
+    headersection = SectionData('header')
+    headersection['title', 'large_text'] = 'TableList'
+    # A textblock contains the widget description
+    ref = "widgets.lists.TableList"
+    headersection['widgetdesc','textblock_ref'] = ref
+    headersection['widgetdesc','text_refnotfound'] = f'Textblock reference {ref} not found'
+    # link to this widgets module page
+    headersection['tomodule','button_text'] = "Module: lists"
+    headersection['tomodule','link_ident'] = skicall.makepath('lists')
+    skicall.update(headersection)
+
+    # this code file contents is placed in section 'codefile' which contains a
+    # PreText widget with name 'pretext'
+    codesection = SectionData('codefile')
+    code = os.path.realpath(__file__)
+    with open(code) as f:
+        codesection['pretext', 'pre_text'] = f.read()
+    skicall.update(codesection)
+
+    # fill in the TableList widget with initial values
+    pd = PageData()
+
+    pd['tablelist','contents'] = [['one', '', '', ''], ['two', '', '', ''], ['three', '', '', ''], ['four', '', '', '']]
+    skicall.update(pd)
+
+
+
+
