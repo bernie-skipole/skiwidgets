add8c17679b1315d88e121d8e8da1fc6d7234764
Bernard Czenkusz <bernie@skipole.co.uk>
2023-01-19 13:49:09 +0000

    Working on TableList widget

--- lists/tablelist.py
+++ lists/tablelist.py
@@ -1,10 +1,11 @@
-import os
+import os, json
 
 from skipole import FailPage, GoTo, ValidateError, ServerError, ServeFile, PageData, SectionData
 
 
-# TABLECONTENTS is a global variable, normally data would be taken from a database
-TABLECONTENTS = ['one', 'two', 'three', 'four']
+# In this example, TABLECONTENTS is a global variable, normally data would be
+# taken from a file or database or other source of data.
+TABLECONTENTS = ['apple', 'pear', 'banana', 'strawberry']
 
 
 def index(skicall):
@@ -32,48 +33,143 @@ def index(skicall):
         codesection['pretext', 'pre_text'] = f.read()
     skicall.update(codesection)
 
-    # fill in the TableList widget with initial values, in this case the items are
-    # taken from TABLECONTENTS, and the get fields describe the row chosen, and the table layout
-    # as a series of indeces of TABLECONTENTS
+    # fill in the TableList widget with initial values, in this case the list items
+    # are taken from TABLECONTENTS, and the get fields for up, down, remove are the
+    # row number of the button pressed
     pd = PageData()
-    pd['tablelist','contents'] = [ [TABLECONTENTS[0], "0_0_1_2_3", "0_0_1_2_3", "0_0_1_2_3"],
-                                   [TABLECONTENTS[1], "1_0_1_2_3", "1_0_1_2_3", "1_0_1_2_3"],
-                                   [TABLECONTENTS[2], "2_0_1_2_3", "2_0_1_2_3", "2_0_1_2_3"],
-                                   [TABLECONTENTS[3], "3_0_1_2_3", "3_0_1_2_3", "3_0_1_2_3"]
+    pd['tablelist','contents'] = [ [TABLECONTENTS[0], "0", "0", "0"],
+                                   [TABLECONTENTS[1], "1", "1", "1"],
+                                   [TABLECONTENTS[2], "2", "2", "2"],
+                                   [TABLECONTENTS[3], "3", "3", "3"]
                                  ]
+
+    # Normally the list would be altered on the source of the data, such as a database.
+    # In this example the list is set onto the client browser, using the ident_data
+    # attribute of pd. The client browser will send this list back when a widget
+    # submits data and it will be available as the skicall.ident_data attribute. So
+    # the list is persistent to the client session.
+    # As ident_data must be a string, this example uses json.dumps to serialise a
+    # list of indexes to the TABLECONTENTS list, rather than the full list itself.
+    pd.ident_data = json.dumps([0,1,2,3])
+    skicall.update(pd)
+
+
+def _createtablecontents(skicall, indexlist):
+    """Takes a list of index numbers, such as [0,1,2,3], these numbers
+       being indexes of the TABLECONTENTS list, and creates the new table
+       contents, then updates skicall"""
+    if not indexlist:
+        pd = PageData()
+        pd['tablelist','contents'] = []
+        pd['tablelist','hide'] = True
+        pd.ident_data = "[]"
+        skicall.update(pd)
+        return
+
+    contents = []
+    for rownumber, itemindex in enumerate(indexlist):
+        rowstring = str(rownumber)
+        rowlist = [TABLECONTENTS[itemindex], rowstring, rowstring, rowstring]
+        contents.append(rowlist)
+    pd = PageData()
+    pd['tablelist','contents'] = contents
+    pd.ident_data = json.dumps(indexlist)
     skicall.update(pd)
 
 
 def up(skicall):
-    "Called to Move the row upwards"
+    "Called to move the row upwards"
+    # the get field of the button pressed is in skicall.call_data
     if ('tablelist', 'contents') not in skicall.call_data:
         raise FailPage(message="No submission received")
+    if skicall.ident_data is None:
+        # no table data has been received
+        raise FailPage(message="Invalid submission received")
 
     index(skicall)
     try:
-        rowlist = list( int(i) for i in skicall.call_data['tablelist', 'contents'].split('_') )
-        # get the row of the table chosen
-        row = rowlist[0]
+        # get the row of the table from the button pressed
+        row = int(skicall.call_data['tablelist', 'contents'])
         if not row:
             # row position is 0, cannot move above row 0
             return
-        # get the table layout
-        table = rowlist[1:]
-        itemtomove = table[row]
-        table.remove(itemtomove)
-        table.insert(row-1, itemtomove)
+
+        indexlist = json.loads(skicall.ident_data)
+        # indexlist is a list such as [0,1,2,3] giving the order of
+        # the indexes of the TABLECONTENTS list
+
+        # move the selected row in the indexlist
+        itemtomove = indexlist[row]
+        indexlist.remove(itemtomove)
+        indexlist.insert(row-1, itemtomove)
     except:
         raise FailPage(message="Invalid submission received")
 
-    tablestring = "_".join(str(i) for i in table)
-    contents = []
-    for rownumber, item in enumerate(table):
-        rowstring = str(rownumber) + "_" + tablestring
-        rowlist = [TABLECONTENTS[item], rowstring, rowstring, rowstring]
-        contents.append(rowlist)
-    pd = PageData()
-    pd['tablelist','contents'] = contents
-    skicall.update(pd)
+    # update the table using the new indexlist
+    _createtablecontents(skicall, indexlist)
+
+
+
+def down(skicall):
+    "Called to move the row downwards"
+    # the get field of the button pressed is in skicall.call_data
+    if ('tablelist', 'contents') not in skicall.call_data:
+        raise FailPage(message="No submission received")
+    if skicall.ident_data is None:
+        # no table data has been received
+        raise FailPage(message="Invalid submission received")
+
+    index(skicall)
+    try:
+        # get the row of the table from the button pressed
+        row = int(skicall.call_data['tablelist', 'contents'])
+
+        indexlist = json.loads(skicall.ident_data)
+        # indexlist is a list such as [0,1,2,3] giving the order of
+        # the indexes of the TABLECONTENTS list
+
+        if row >= len(indexlist) - 1:
+            # row is at the last position, cannot move further down
+            return
+
+        # move the selected row in the indexlist
+        itemtomove = indexlist[row]
+        indexlist.remove(itemtomove)
+        indexlist.insert(row+1, itemtomove)
+    except:
+        raise FailPage(message="Invalid submission received")
+
+    # update the table using the new indexlist
+    _createtablecontents(skicall, indexlist)
+
+
+
+def remove(skicall):
+    "Called to remove a row"
+    # the get field of the button pressed is in skicall.call_data
+    if ('tablelist', 'contents') not in skicall.call_data:
+        raise FailPage(message="No submission received")
+    if skicall.ident_data is None:
+        # no table data has been received
+        raise FailPage(message="Invalid submission received")
+
+    index(skicall)
+    try:
+        # get the row of the table from the button pressed
+        row = int(skicall.call_data['tablelist', 'contents'])
+
+        indexlist = json.loads(skicall.ident_data)
+        # indexlist is a list such as [0,1,2,3] giving the order of
+        # the indexes of the TABLECONTENTS list
+
+        # remove the selected row in the indexlist
+        itemtoremove = indexlist[row]
+        indexlist.remove(itemtoremove)
+    except:
+        raise FailPage(message="Invalid submission received")
+
+    # update the table using the new indexlist
+    _createtablecontents(skicall, indexlist)
 
 
 
