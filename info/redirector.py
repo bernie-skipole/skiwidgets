9811e0ce4c5e59da9ee916f93dfb71197ca997f0
bernie-skipole <bernie@skipole.co.uk>
2023-02-10 12:50:50 +0000

    working on redirector

---
+++ info/redirector.py
@@ -0,0 +1,40 @@
+import os
+
+from skipole import FailPage, GoTo, ValidateError, ServerError, ServeFile, PageData, SectionData
+
+
+def index(skicall):
+    "Called by a SubmitData responder, and sets up the page"
+
+    # the title and widget decription is in section 'header' which contains a
+    # HeadText widget with name 'title' and a TextBlockPara widget with name 'widgetdesc'
+    # It also has a ButtonLink2 widget with name 'tomodule'
+    headersection = SectionData('header')
+    headersection['title', 'large_text'] = 'Redirector'
+    # A textblock contains the widget description
+    ref = "widgets.info.Redirector"
+    headersection['widgetdesc','textblock_ref'] = ref
+    headersection['widgetdesc','text_refnotfound'] = f'Textblock reference {ref} not found'
+    # link to this widgets module page
+    headersection['tomodule','button_text'] = "Module: info"
+    headersection['tomodule','link_ident'] = skicall.makepath('info')
+    skicall.update(headersection)
+
+    # this code file contents is placed in section 'codefile' which contains a
+    # PreText widget with name 'pretext'
+    codesection = SectionData('codefile')
+    code = os.path.realpath(__file__)
+    with open(code) as f:
+        codesection['pretext', 'pre_text'] = f.read()
+    skicall.update(codesection)
+
+
+def submit_redirection(skicall):
+    "Called to submit a redirection URL"
+
+    if ('submittext', 'input_text') not in skicall.call_data:
+        raise FailPage(message="No submission received")
+
+    pd = PageData()
+    pd["redirector","url"] = skicall.call_data['submittext', 'input_text']
+    skicall.update(pd)
