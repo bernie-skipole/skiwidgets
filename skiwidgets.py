e49463f934b48abc2c0df99d36c83a22f3ae43d3
Bernard Czenkusz <bernie@skipole.co.uk>
2023-03-07 16:27:03 +0000

    added filelist widget

--- skiwidgets.py
+++ skiwidgets.py
@@ -1,6 +1,10 @@
 
 
-import os
+import os, sys
+
+# for local development by bernie
+#sys.path.insert(0, "/home/bernard/git/skipole")
+#sys.path.insert(0, "/home/bernard/git/skilift")
 
 from skipole import WSGIApplication, FailPage, GoTo, ValidateError, ServerError, ServeFile, use_submit_list, skis, PageData, SectionData, set_debug, widget_modules
 
